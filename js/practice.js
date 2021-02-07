f58ea2394797a917a59158cbaa29aabc54705da0
Baruch Karlin <76958079+Baruch-Karlin@users.noreply.github.com>
2021-02-07 13:27:16 +0200

    create html file

---
+++ js/practice.js
@@ -0,0 +1,15 @@
+let dayOfWeek = "sunday";
+switch (dayOfWeek) {
+    case "sunday":
+        console.log("Today is Sunday");
+    
+    case "Monday":
+    console.log("today is monday");
+    
+    
+       
+        default:
+            console.log("other day");
+            break;
+
+}
